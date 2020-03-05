439dc4e2be5db577b011b2689172428c2c507198
hamnord <h.nordenstein@hotmail.com>
2020-03-05 15:48:42 +0100

    meh

--- DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/CreatePlayer.java
+++ DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/CreatePlayer.java
@@ -1,4 +1,39 @@
 package com.example.DeluxeRps.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 public class CreatePlayer {
+
+  final FileChooser fileChooser = new FileChooser();
+
+  @FXML
+  public void addProfilePicture (MouseEvent mouseEvent) throws MalformedURLException {
+    Stage stage = new Stage();
+    File file = fileChooser.showOpenDialog(stage);
+    if (file != null) {
+
+      Image img = new Image(file.toURI().toURL().toString());
+      ImageView imageView = new ImageView();
+      imageView.setImage(img);
+
+    }
+  }
+
+
+
+  public void confirmButtonClicked(MouseEvent mouseEvent) throws IOException {
+    Helper.replaceScene(Helper.selectPlayerModeFXML,Helper.selectPlayerModeTitle, mouseEvent);
+  }
+
+
 }
