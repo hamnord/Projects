8973a05e40d50222bcb0c2a02869c06f4091a9d0
hamnord <h.nordenstein@hotmail.com>
2020-03-17 11:31:10 +0100

    bruh

--- DeluxeRps/src/main/java/com/example/DeluxeRps/Main.java
+++ DeluxeRps/src/main/java/com/example/DeluxeRps/Main.java
@@ -10,6 +10,7 @@ import com.example.DeluxeRps.Controllers.Helper;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -21,11 +22,11 @@ public class Main extends Application {
   public Connection connection;
 
   @Override
-  public void start(Stage mainMenu) throws IOException {
+  public void start(Stage mainMenu) throws IOException, SQLException {
 
     try {
       Class.forName("org.postgresql.Driver");
-      connection = DriverManager.getConnection("postgres://igblmsacvvtqrc:8aa6d775c64cc09d4e2aee35743c2ed90290530663b15d687f0e4bfff5542a68@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d2621gbprb812i");
+      connection = DriverManager.getConnection("jdbc:postgresql://ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d2621gbprb812i", "igblmsacvvtqrc", "8aa6d775c64cc09d4e2aee35743c2ed90290530663b15d687f0e4bfff5542a68");
       connection.setAutoCommit(false);
 
     } catch (Exception e) {
