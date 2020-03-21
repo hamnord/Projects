0fe1c9f7693425c367db23c03585a62296dc0b83
heimgun <heidi.guneriussen@live.no>
2020-03-21 18:58:00 +0100

    Login accually works damn

--- DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/CreatePlayer.java
+++ DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/CreatePlayer.java
@@ -39,8 +39,7 @@ public class CreatePlayer {
     return b64Encode.encodeToString(randomBytes);
   }
 
-
-
+  //FXML-Objects
   @FXML
   private TextField newUserName;
   @FXML
@@ -50,86 +49,95 @@ public class CreatePlayer {
   @FXML
   private TextField newEmail;
 
+  //Other useful objects
   String username, password, repeatPass, email;
 
 
 
   public void confirmButtonClicked(MouseEvent mouseEvent) throws IOException, SQLException {
 
+    //Getting input
     username = newUserName.getText();
     password = newPassword.getText();
     repeatPass = repeatPassword.getText();
     email = newEmail.getText();
 
+
+    //Get Connection
     try {
           Class.forName("org.postgresql.Driver");
           connection = DriverManager.getConnection("jdbc:postgresql://ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d2621gbprb812i", "igblmsacvvtqrc", "8aa6d775c64cc09d4e2aee35743c2ed90290530663b15d687f0e4bfff5542a68");
         } catch (Exception e) {e.printStackTrace();}
 
-
+    //Prep statement
     try {
-
       existingUserStmnt = connection.prepareStatement("SELECT * FROM gamedb.users WHERE username = ?");
       existingUserStmnt.setString(1, username);
       ResultSet storedUser = existingUserStmnt.executeQuery();
 
-
-
-      if (storedUser.next()){
+     //If user exists in users-table
+     if (storedUser.next()){
         Alert alert = new Alert(AlertType.NONE, "Error! this user already exists", ButtonType.OK);
         alert.setTitle("Error in creating user");
         alert.show();
       }
+
       else {
 
-        if (username.equals("")) {
+          //Exceptions for input
+          if (username.equals("")) {
           Alert alert = new Alert(AlertType.NONE, "Error! Username field are empty", ButtonType.OK);
           alert.setTitle("Error in creating user");
           alert.show();
-        }
-
-      else  if (password.equals("")) {
+          }
+          else if (password.equals("")) {
           Alert alert = new Alert(AlertType.NONE, "Error! Password field is empty", ButtonType.OK);
           alert.setTitle("Error in creating user");
           alert.show();
-        }
-
-      else  if (!password.equals(repeatPass)) {
+          }
+          else if (!password.equals(repeatPass)) {
           Alert alert = new Alert(AlertType.NONE, "Error! password fields does not match", ButtonType.OK);
           alert.setTitle("Error in creating user");
           alert.show();
-        }
-      else if (email.equals("")) {
+          }
+          else if (email.equals("")) {
           Alert alert = new Alert(AlertType.NONE, "Error! Email field is empty", ButtonType.OK);
           alert.setTitle("Error in creating user");
           alert.show();
-        } else {
-          prepStmnt = connection.prepareStatement("INSERT INTO gamedb.users (username, password, email) VALUES (?,?,?) ");
+          }
+
+          //Adding user to DB
+          else {
+          prepStmnt = connection.prepareStatement("INSERT INTO gamedb.users (username, password, email) VALUES (?,?,?)");
           prepStmnt.setString(1, username);
           prepStmnt.setString(2, password);
           prepStmnt.setString(3, email);
           prepStmnt.executeUpdate();
-        }
+          }
 
+
+
+        //Not sure whats happening here, cant get tokens to work/not sure how
         String userToken = generateToken();
 
-        userIDStmnt = connection.prepareStatement("Select userid FROM gamedb.users WHERE username = ? AND password = ?");
+        userIDStmnt = connection.prepareStatement("SELECT userid FROM gamedb.users WHERE username = ? AND password = ?");
         userIDStmnt.setString(1, username);
         userIDStmnt.setString(2, password);
         ResultSet userid = userIDStmnt.executeQuery();
         int useridInt = userid.getInt("userid");
-
-
 
         tokenStmnt = connection.prepareStatement("INSERT INTO gamedb.tokens (tokenid, userid) VALUES (?, ?)");
         tokenStmnt.setString(1, userToken);
         tokenStmnt.setInt(2, useridInt);
         tokenStmnt.executeUpdate();
 
-
         Helper.replaceScene(Helper.selectPlayerModeFXML, Helper.selectPlayerModeTitle, mouseEvent);
+
+
       }
     }
+
+    //Exceptions
     catch (Exception e) {
         e.printStackTrace();
       } finally {
@@ -144,12 +152,14 @@ public class CreatePlayer {
     }
 
 
+  //Back-Button, works great
   public void backButtonClicked (MouseEvent mouseEvent) throws IOException {
     Helper.replaceScene(Helper.mainMenuFXML, Helper.mainMenuTitle, mouseEvent);
   }
 
 
   // TODO just fix this shit when there is time bruh
+  //Youll fix it bruh, I believe in ya
   @FXML
   public void addProfilePicture (MouseEvent mouseEvent) throws MalformedURLException {
     Stage stage = new Stage();
--- DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/Login.java
+++ DeluxeRps/src/main/java/com/example/DeluxeRps/Controllers/Login.java
@@ -1,17 +1,17 @@
 package com.example.DeluxeRps.Controllers;
 
 import com.example.DeluxeRps.Models.ConnectionMaster;
+import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.security.SecureRandom;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Base64;
 
 public class Login {
@@ -22,6 +22,13 @@ public class Login {
   public PreparedStatement loginStmt;
   private String username;
   private String password;
+
+
+  //FXML-Objects
+  @FXML
+  TextField userNameInput;
+  @FXML
+  PasswordField passwordField;
 
 
   public static String generateToken (){
@@ -37,53 +44,55 @@ public class Login {
   }
 
 
-/*
-  // säker men skapar kaos i koden
-  public static void insertToken2 (String token, String username, String password) throws SQLException {
-    PreparedStatement state;
-
-    Connection connection;
-    state = connection.prepareStatement("INSERT INTO \"token\" (\"token_id\") VALUES (" + token + " ) " +
-        "INNER JOIN user WHERE \"user_name\"=? and \"password\"= ?");
-    state.setString(1,username);
-    state.setString(2,password);
-    state.executeQuery();
-  }*/
 
   public void loginButtonClicked (MouseEvent mouseEvent) throws IOException, SQLException {
 
-    try {
+    //Getting input
+    username = userNameInput.getText();
+    password = passwordField.getText();
 
-      loginStmt = connection.prepareStatement("Select * from \"users\" where \"username\" = ? and \"password\" = ?;");
+    //Get Connection
+    try {
+      Class.forName("org.postgresql.Driver");
+      connection = DriverManager.getConnection("jdbc:postgresql://ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/d2621gbprb812i", "igblmsacvvtqrc", "8aa6d775c64cc09d4e2aee35743c2ed90290530663b15d687f0e4bfff5542a68");
+    } catch (Exception e) {e.printStackTrace();}
+
+    //Check for user in DB
+    try {
+      loginStmt = connection.prepareStatement("SELECT * FROM gamedb.users WHERE username = ? AND password = ?");
       loginStmt.setString(1, username);
       loginStmt.setString(2, password);
       ResultSet validUser = loginStmt.executeQuery();
 
-
-      if (validUser == null){
+        //Exception-alerts
+        if (validUser == null){
         Alert alert = new Alert(AlertType.NONE, " Error! Credential fields are empty", ButtonType.OK);
         alert.setTitle("Error in authentication");
         alert.show();
-      }
-      else if (!validUser.next()){
+        }
+        else if (!validUser.next()){
         Alert alert = new Alert(AlertType.NONE, " Error! Credentials does not match", ButtonType.OK);
         alert.setTitle("Error in authentication");
         alert.show();
-      }
-      else{
+        }
+
+        //Logged in! Tokens wont work + also maybe add user til listofloggedinusers
+        else{
         System.out.println("Authentication successful");
         generateToken();
         insertToken(generateToken(), username, password);
         Helper.replaceScene(Helper.selectPlayerModeFXML, Helper.selectPlayerModeTitle, mouseEvent);
-      }
+        }
 
     }
+
+    //Exceptions
     catch (SQLException e) {
       e.printStackTrace();
     }
     }
 
-
+  //Back-Button works great
   public void backButtonClicked (MouseEvent mouseEvent) throws IOException {
     Helper.replaceScene(Helper.mainMenuFXML, Helper.mainMenuTitle, mouseEvent);
   }
